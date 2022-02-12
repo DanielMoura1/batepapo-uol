ef17cdde742c117217dafc23050478d8e52dc84a
Daniel <daniel.bnh13gmail.com>
2022-02-12 02:34:45 -0300

    ainda ta feio

---
+++ codigo.js
@@ -0,0 +1,198 @@
+let i=0
+let nomeTeste=''
+let textoTeste=''
+let timeTeste=''
+let mensagem = document.querySelector("input").value;
+let nome=''
+
+
+function nome2(){
+    nome = prompt('insira nome')
+    entrar()
+}
+nome2()
+let para= "Todos"
+let type = "message"
+
+
+servidor()
+selecionarfrom()
+function  mandarMensagem(){
+    
+    let TEXTO = document.querySelector("input").value;
+    console.log('TEXTO'+TEXTO)
+        
+        //----------------------------------------------------------------
+
+        const promessaMandar = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',{
+
+            from: nome,
+            to: para,
+            text: TEXTO,
+            type: type // ou "private_message" para o bônus
+        })
+
+    servidor()
+}
+function menu(){
+    const imgg=document.querySelector('.caixamenu')
+    imgg.classList.toggle('barra')
+    
+}
+function menuSeta(eu,name1){
+    const seta =document.querySelector('.opcoesCaixa .selecionado')
+    
+    if(seta !== null){
+       
+        seta.classList.remove('selecionado')
+    }
+   
+    eu.classList.add('selecionado')
+    para=name1
+    alert(para)
+}
+function menuSeta2(eu,pegar){
+    const seta =document.querySelector('.opcoesCaixa2 .selecionado')
+    
+    if(seta !== null){
+       
+        seta.classList.remove('selecionado')
+    }
+   
+    eu.classList.add('selecionado')
+    type = pegar
+    alert(type)
+}
+function entrar(){
+    
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',
+    {
+        name: nome
+    })
+    promessa.catch(tratarError);
+    
+}
+function servidor(){
+    // pegar coisas 
+    const promessaPegar =axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promessaPegar.then(pegador)
+    
+    //madar coisas
+    
+    
+   
+
+}
+
+
+function pegador(pegar){
+   console.log(pegar)
+   
+    
+        let from = pegar.data[99].from
+        let axt =pegar.data[99].text;
+        let time =pegar.data[99].time
+        let to =pegar.data[99].to
+        let type =pegar.data[99].type
+        
+        console.log('pq?'+ axt)
+    if((to=='Todos' || to == nome || from == nome) &&(textoTeste !== axt || nomeTeste !== from || timeTeste !== timeTeste)){ 
+        
+        
+        console.log(textoTeste +' = '+axt+' e '+ nomeTeste+" = "+from )
+        let ul = document.querySelector("ul");
+        ul.scrollIntoView(false);
+        if(axt =='entra na sala...'|| axt=='sai da sala...'){
+        ul.innerHTML += `
+                <div id='okk' class="caixaDeTexto cinza"><p>${time}  ${from} para ${to} : ${axt}</p></div>
+    
+        `
+        }else if(type =='private_message'){
+            ul.innerHTML += `
+                <div id='okk' class="caixaDeTexto rosa"><p>${time}  ${from} para ${to} : ${axt}</p></div>
+    
+        `
+
+        }else{
+            ul.innerHTML += `
+                <div id='okk' class="caixaDeTexto branco"><p>${time}  ${from} para ${to} : ${axt}</p></div>
+    
+        `
+
+        }
+        
+        
+        nomeTeste = from
+        textoTeste = axt
+        timeTeste = time
+        
+    }
+    
+   
+}
+function tratarError(erro){
+    nome2()
+}
+
+function online(){
+    const promessa2 = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',
+    {
+        name: nome
+    })
+}
+function confirmadorOnline(){
+    setInterval(online,1000);
+}
+function TIME(){
+    setInterval(servidor,100);
+}
+function TIME2(){
+    setInterval(selecionarfrom,10000);
+}
+function selecionarfrom(){
+    const promessaPegar =axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promessaPegar.then(selecionarPesoa)
+    
+}
+
+function selecionarPesoa(pegar){
+    
+    
+    let ull = document.querySelector(".teste");
+    ull.innerHTML=''
+    const lista =[]
+    for(let a=0;a<100;a++){
+        let j= 0
+        let nomee= pegar.data[a].from
+        ok = true
+        while(j<lista.length){
+            if(nomee==lista[j]){
+                ok =false
+                break
+            }
+            j++
+
+        }
+        if(ok){
+            
+            ull.innerHTML +=`<div class="opcoes"  onclick=" menuSeta(this, '${nomee}')">
+                <div class="amarrar">
+                    <img src="Vector.png">
+                    <p>
+                        ${nomee}
+                    </p>
+                    <ion-icon class="seta" name="checkmark-sharp"></ion-icon>
+                </div>
+                
+            </div >` 
+            lista.push(nomee)
+        }
+
+    }
+}
+
+
+
+confirmadorOnline()
+TIME()
+TIME2()
